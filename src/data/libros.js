ff8b4b0f7bf1a1a09734701c0a4411120fd28d27
Jose <sissamendoza@outlook.com>
2024-07-23 10:21:08 -0500

    Ajustando las páginas dinámicas

--- src/data/libros.js
+++ src/data/libros.js
@@ -2,7 +2,8 @@ export const LIBROS_NEW = [
     {
         id: "grado-6",
         grado: "Grado 6",
-        profesor: "Profeso: 6",
+        profesor: "Profesor: 6",
+        tema: "Villa del Rosario / Tradicional",
         libros: [
             {
                 id: "6-1",
@@ -11,7 +12,7 @@ export const LIBROS_NEW = [
                 anio: "2000",
                 autor: "Brandon Sanderson",
                 genero: ["comedia", "horror"],
-                imagen_libro: "libro-1",
+                imagen_libro: "con-pombo-y-platillos",
                 comentarios: [],
             },
             {
@@ -37,7 +38,8 @@ export const LIBROS_NEW = [
             {
                 id: "6-4",
                 nombre: "El Hombre Iluminado",
-                descripcion: "Hace años tenía compañeros de armas y una causa en la que creer, pero ahora el hombre que se hace llamar Nómada solo conoce la vida a la fuga. Obligado a saltar de un mundo a otro en el Cosmere cada vez que la implacable Brigada Nocturna le gana demasiado terreno, Nómada acaba en un nuevo planeta y de inmediato se ve implicado en la trifulca entre un tirano y los rebeldes que solo pretenden evitar que los conviertan en esclavos sin mente. Y todo bajo la amenaza constante de un amanecer cuyo calor derrite la misma piedra. Incapaz de comprender su idioma, ¿sabrá navegar el conflicto y obtener el suficiente poder para saltar fuera del planeta antes de que su mente o su cuerpo paguen el precio definitivo?",
+                descripcion:
+                    "Hace años tenía compañeros de armas y una causa en la que creer, pero ahora el hombre que se hace llamar Nómada solo conoce la vida a la fuga. Obligado a saltar de un mundo a otro en el Cosmere cada vez que la implacable Brigada Nocturna le gana demasiado terreno, Nómada acaba en un nuevo planeta y de inmediato se ve implicado en la trifulca entre un tirano y los rebeldes que solo pretenden evitar que los conviertan en esclavos sin mente. Y todo bajo la amenaza constante de un amanecer cuyo calor derrite la misma piedra. Incapaz de comprender su idioma, ¿sabrá navegar el conflicto y obtener el suficiente poder para saltar fuera del planeta antes de que su mente o su cuerpo paguen el precio definitivo?",
                 anio: "2000",
                 autor: "Brandon Sanderson",
                 genero: ["comedia", "horror"],
@@ -80,7 +82,8 @@ export const LIBROS_NEW = [
     {
         id: "grado-7",
         grado: "Grado 7",
-        profesor: "Profeso: 7",
+        profesor: "Profesor: 7",
+        tema: "Mitos y Leyendas",
         libros: [
             {
                 id: "7-1",
@@ -115,7 +118,8 @@ export const LIBROS_NEW = [
             {
                 id: "7-4",
                 nombre: "El Hombre Iluminado",
-                descripcion: "Hace años tenía compañeros de armas y una causa en la que creer, pero ahora el hombre que se hace llamar Nómada solo conoce la vida a la fuga. Obligado a saltar de un mundo a otro en el Cosmere cada vez que la implacable Brigada Nocturna le gana demasiado terreno, Nómada acaba en un nuevo planeta y de inmediato se ve implicado en la trifulca entre un tirano y los rebeldes que solo pretenden evitar que los conviertan en esclavos sin mente. Y todo bajo la amenaza constante de un amanecer cuyo calor derrite la misma piedra. Incapaz de comprender su idioma, ¿sabrá navegar el conflicto y obtener el suficiente poder para saltar fuera del planeta antes de que su mente o su cuerpo paguen el precio definitivo?",
+                descripcion:
+                    "Hace años tenía compañeros de armas y una causa en la que creer, pero ahora el hombre que se hace llamar Nómada solo conoce la vida a la fuga. Obligado a saltar de un mundo a otro en el Cosmere cada vez que la implacable Brigada Nocturna le gana demasiado terreno, Nómada acaba en un nuevo planeta y de inmediato se ve implicado en la trifulca entre un tirano y los rebeldes que solo pretenden evitar que los conviertan en esclavos sin mente. Y todo bajo la amenaza constante de un amanecer cuyo calor derrite la misma piedra. Incapaz de comprender su idioma, ¿sabrá navegar el conflicto y obtener el suficiente poder para saltar fuera del planeta antes de que su mente o su cuerpo paguen el precio definitivo?",
                 anio: "2000",
                 autor: "Brandon Sanderson",
                 genero: ["comedia", "horror"],
@@ -158,7 +162,8 @@ export const LIBROS_NEW = [
     {
         id: "grado-8",
         grado: "Grado 8",
-        profesor: "Profeso: 8",
+        profesor: "Profesor: 8",
+        tema: "Obras Colombianas",
         libros: [
             {
                 id: "8-1",
@@ -193,7 +198,8 @@ export const LIBROS_NEW = [
             {
                 id: "8-4",
                 nombre: "El Hombre Iluminado",
-                descripcion: "Hace años tenía compañeros de armas y una causa en la que creer, pero ahora el hombre que se hace llamar Nómada solo conoce la vida a la fuga. Obligado a saltar de un mundo a otro en el Cosmere cada vez que la implacable Brigada Nocturna le gana demasiado terreno, Nómada acaba en un nuevo planeta y de inmediato se ve implicado en la trifulca entre un tirano y los rebeldes que solo pretenden evitar que los conviertan en esclavos sin mente. Y todo bajo la amenaza constante de un amanecer cuyo calor derrite la misma piedra. Incapaz de comprender su idioma, ¿sabrá navegar el conflicto y obtener el suficiente poder para saltar fuera del planeta antes de que su mente o su cuerpo paguen el precio definitivo?",
+                descripcion:
+                    "Hace años tenía compañeros de armas y una causa en la que creer, pero ahora el hombre que se hace llamar Nómada solo conoce la vida a la fuga. Obligado a saltar de un mundo a otro en el Cosmere cada vez que la implacable Brigada Nocturna le gana demasiado terreno, Nómada acaba en un nuevo planeta y de inmediato se ve implicado en la trifulca entre un tirano y los rebeldes que solo pretenden evitar que los conviertan en esclavos sin mente. Y todo bajo la amenaza constante de un amanecer cuyo calor derrite la misma piedra. Incapaz de comprender su idioma, ¿sabrá navegar el conflicto y obtener el suficiente poder para saltar fuera del planeta antes de que su mente o su cuerpo paguen el precio definitivo?",
                 anio: "2000",
                 autor: "Brandon Sanderson",
                 genero: ["comedia", "horror"],
@@ -236,7 +242,8 @@ export const LIBROS_NEW = [
     {
         id: "grado-9",
         grado: "Grado 9",
-        profesor: "Profeso: 9",
+        profesor: "Profesor: 9",
+        tema: "Obras Latinoamericanas",
         libros: [
             {
                 id: "9-1",
@@ -271,7 +278,8 @@ export const LIBROS_NEW = [
             {
                 id: "9-4",
                 nombre: "El Hombre Iluminado",
-                descripcion: "Hace años tenía compañeros de armas y una causa en la que creer, pero ahora el hombre que se hace llamar Nómada solo conoce la vida a la fuga. Obligado a saltar de un mundo a otro en el Cosmere cada vez que la implacable Brigada Nocturna le gana demasiado terreno, Nómada acaba en un nuevo planeta y de inmediato se ve implicado en la trifulca entre un tirano y los rebeldes que solo pretenden evitar que los conviertan en esclavos sin mente. Y todo bajo la amenaza constante de un amanecer cuyo calor derrite la misma piedra. Incapaz de comprender su idioma, ¿sabrá navegar el conflicto y obtener el suficiente poder para saltar fuera del planeta antes de que su mente o su cuerpo paguen el precio definitivo?",
+                descripcion:
+                    "Hace años tenía compañeros de armas y una causa en la que creer, pero ahora el hombre que se hace llamar Nómada solo conoce la vida a la fuga. Obligado a saltar de un mundo a otro en el Cosmere cada vez que la implacable Brigada Nocturna le gana demasiado terreno, Nómada acaba en un nuevo planeta y de inmediato se ve implicado en la trifulca entre un tirano y los rebeldes que solo pretenden evitar que los conviertan en esclavos sin mente. Y todo bajo la amenaza constante de un amanecer cuyo calor derrite la misma piedra. Incapaz de comprender su idioma, ¿sabrá navegar el conflicto y obtener el suficiente poder para saltar fuera del planeta antes de que su mente o su cuerpo paguen el precio definitivo?",
                 anio: "2000",
                 autor: "Brandon Sanderson",
                 genero: ["comedia", "horror"],
@@ -315,6 +323,7 @@ export const LIBROS_NEW = [
         id: "grado-10",
         grado: "Grado 10",
         profesor: "Profesor: 10",
+        tema: "Obras Españolas",
         libros: [
             {
                 id: "10-1",
@@ -349,7 +358,8 @@ export const LIBROS_NEW = [
             {
                 id: "10-4",
                 nombre: "El Hombre Iluminado",
-                descripcion: "Hace años tenía compañeros de armas y una causa en la que creer, pero ahora el hombre que se hace llamar Nómada solo conoce la vida a la fuga. Obligado a saltar de un mundo a otro en el Cosmere cada vez que la implacable Brigada Nocturna le gana demasiado terreno, Nómada acaba en un nuevo planeta y de inmediato se ve implicado en la trifulca entre un tirano y los rebeldes que solo pretenden evitar que los conviertan en esclavos sin mente. Y todo bajo la amenaza constante de un amanecer cuyo calor derrite la misma piedra. Incapaz de comprender su idioma, ¿sabrá navegar el conflicto y obtener el suficiente poder para saltar fuera del planeta antes de que su mente o su cuerpo paguen el precio definitivo?",
+                descripcion:
+                    "Hace años tenía compañeros de armas y una causa en la que creer, pero ahora el hombre que se hace llamar Nómada solo conoce la vida a la fuga. Obligado a saltar de un mundo a otro en el Cosmere cada vez que la implacable Brigada Nocturna le gana demasiado terreno, Nómada acaba en un nuevo planeta y de inmediato se ve implicado en la trifulca entre un tirano y los rebeldes que solo pretenden evitar que los conviertan en esclavos sin mente. Y todo bajo la amenaza constante de un amanecer cuyo calor derrite la misma piedra. Incapaz de comprender su idioma, ¿sabrá navegar el conflicto y obtener el suficiente poder para saltar fuera del planeta antes de que su mente o su cuerpo paguen el precio definitivo?",
                 anio: "2000",
                 autor: "Brandon Sanderson",
                 genero: ["comedia", "horror"],
@@ -393,75 +403,50 @@ export const LIBROS_NEW = [
         id: "grado-11",
         grado: "Grado 11",
         profesor: "Profesor: 11",
+        tema: "Obras Universales",
         libros: [
             {
                 id: "11-1",
-                nombre: "The Effective Executive 11",
-                descripcion: "Descripción del libro",
-                anio: "2000",
-                autor: "Brandon Sanderson",
-                genero: ["comedia", "horror"],
-                imagen_libro: "libro-1",
+                nombre: "Don Quijote de la Mancha",
+                descripcion:
+                    "Capítulos I y VIII de la novela con las páginas más hermosas de cuantas se han escrito en español. Descubre la historia que inicia con la famosa frase: “En un lugar de La Mancha, de cuyo nombre no quiero acordarme, no ha mucho tiempo que vivía un hidalgo de los de lanza en astillero…”",
+                anio: "1605, Primera parte.",
+                autor: "Miguel de Cervantes Saavedra",
+                genero: ["Novela", "Historieta", "Sátira"],
+                imagen_libro: "obras-universales/don-quijote-de-la-mancha",
                 comentarios: [],
             },
             {
                 id: "11-2",
-                nombre: "Atomic Habits",
-                descripcion: "Descripción del libro",
-                anio: "2000",
-                autor: "Brandon Sanderson",
-                genero: ["comedia", "horror"],
-                imagen_libro: "libro-2",
+                nombre: "Romeo y Julieta",
+                descripcion:
+                    "Esta obra maestra de William Shakespeare, genio de la lengua inglesa, es la historia de amor más hermosa y trágica que jamás se haya escrito. Esta versión de los hermanos Lamb ha sido especialmente escrita para los jóvenes. No dudamos de que su lectura proporcionará, además de un rato de esparcimiento, enseñanzas muy profundas.",
+                anio: "1597",
+                autor: "William Shakespeare",
+                genero: ["Tragedia", "Drama Romántico"],
+                imagen_libro: "obras-universales/romeo-y-julieta",
                 comentarios: [],
             },
             {
                 id: "11-3",
-                nombre: "Radical Candor",
-                descripcion: "Descripción del libro",
-                anio: "2000",
-                autor: "Brandon Sanderson",
-                genero: ["comedia", "horror"],
-                imagen_libro: "libro-3",
+                nombre: "Don Quijote de la Mancha",
+                descripcion:
+                    "Capítulos I y VIII de la novela con las páginas más hermosas de cuantas se han escrito en español. Descubre la historia que inicia con la famosa frase: “En un lugar de La Mancha, de cuyo nombre no quiero acordarme, no ha mucho tiempo que vivía un hidalgo de los de lanza en astillero…”",
+                anio: "1605, Primera parte.",
+                autor: "Miguel de Cervantes Saavedra",
+                genero: ["Novela", "Historieta", "Sátira"],
+                imagen_libro: "obras-universales/don-quijote-de-la-mancha",
                 comentarios: [],
             },
             {
                 id: "11-4",
-                nombre: "El Hombre Iluminado",
-                descripcion: "Hace años tenía compañeros de armas y una causa en la que creer, pero ahora el hombre que se hace llamar Nómada solo conoce la vida a la fuga. Obligado a saltar de un mundo a otro en el Cosmere cada vez que la implacable Brigada Nocturna le gana demasiado terreno, Nómada acaba en un nuevo planeta y de inmediato se ve implicado en la trifulca entre un tirano y los rebeldes que solo pretenden evitar que los conviertan en esclavos sin mente. Y todo bajo la amenaza constante de un amanecer cuyo calor derrite la misma piedra. Incapaz de comprender su idioma, ¿sabrá navegar el conflicto y obtener el suficiente poder para saltar fuera del planeta antes de que su mente o su cuerpo paguen el precio definitivo?",
-                anio: "2000",
-                autor: "Brandon Sanderson",
-                genero: ["comedia", "horror"],
-                imagen_libro: "libro-4",
-                comentarios: [],
-            },
-            {
-                id: "11-5",
-                nombre: "La Rueda del Tiempo 12",
-                descripcion: "Descripción del libro",
-                anio: "2000",
-                autor: "Brandon Sanderson",
-                genero: ["comedia", "horror"],
-                imagen_libro: "libro-5",
-                comentarios: [],
-            },
-            {
-                id: "11-6",
-                nombre: "La Rueda del Tiempo 14",
-                descripcion: "Descripción del libro",
-                anio: "2000",
-                autor: "Brandon Sanderson",
-                genero: ["comedia", "horror"],
-                imagen_libro: "libro-6",
-                comentarios: [],
-            },
-            {
-                id: "11-7",
-                nombre: "La Rueda del Tiempo 14",
-                descripcion: "Descripción del libro",
-                anio: "2000",
-                autor: "Brandon Sanderson",
-                genero: ["comedia", "horror"],
-                imagen_libro: "libro-7",
+                nombre: "Romeo y Julieta",
+                descripcion:
+                    "Esta obra maestra de William Shakespeare, genio de la lengua inglesa, es la historia de amor más hermosa y trágica que jamás se haya escrito. Esta versión de los hermanos Lamb ha sido especialmente escrita para los jóvenes. No dudamos de que su lectura proporcionará, además de un rato de esparcimiento, enseñanzas muy profundas.",
+                anio: "1597",
+                autor: "William Shakespeare",
+                genero: ["Tragedia", "Drama Romántico"],
+                imagen_libro: "obras-universales/romeo-y-julieta",
                 comentarios: [],
             },
         ],
