490d8d9c6982a3cf28806b7669c9aba536b55081
Jose <sissamendoza@outlook.com>
2024-08-19 15:42:02 -0500

    Astro updated

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,6 +4,6 @@ import vercel from "@astrojs/vercel/serverless";
 
 // https://astro.build/config
 export default defineConfig({
-    output: "server",
-    adapter: vercel(),
+    output: "static",
+    // adapter: vercel(),
 });
